c78a2bbe4d7baa05c060856136ac761f0a88f614
Unlasting-Q <821496783@qq.com>
2021-08-13 11:24:37 +0800

    Site updated: 2021-08-13 11:24:34

---
+++ js/aplayer.js
@@ -0,0 +1,24 @@
+!(function() {
+  var oldLoadAp = window.onload;
+  window.onload = function () {
+    oldLoadAp && oldLoadAp();
+
+    new APlayer({
+      container: document.getElementById('aplayer'),
+      fixed: true,
+      autoplay: false,
+      loop: 'all',
+      order: 'random',
+      theme: '#b7daff',
+      preload: 'none',
+      audio: [
+        {
+          name: 'Aruarian Dance',
+          artist: 'Nujabes',
+          url: '/songs/song1.mp3',
+          cover: '/img/Nujabes.png'
+        }
+      ]
+    });
+  }
+})();
